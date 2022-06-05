ab8f10a9052613417e5d082eb04502a4763530be
Melon <99061680+Mel0n7@users.noreply.github.com>
2022-06-05 02:08:18 +0000

    Did some database creation stuff

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import logging, sys, itertools
-from flask import Flask, render_template
+import logging, itertools, sqlite3, os, base64
+from flask import Flask
 
-version = "Alpha 0.0.1"
+if os.path.exists("./game.db"):
+  conn = sqlite3.connect("game.db")
+  cursor = conn.cursor()
+version = "Alpha 0.0.2"
 game_version = "2.1"
 l = True # Turns Logging on and off
 
@@ -22,11 +25,36 @@ def home():
 def start_server():
   app.run("0.0.0.0", port=80)
 
+def reset_database():
+  print("IF YOU ALREADY HAVE A DATABASE IT WILL BE CLEARED. DO YOU STILL WANT CONTINUE? [Y/N]")
+  awnser = input()
+  if awnser == "Y" or awnser == "y":
+    if os.path.exists("./game.db"):
+      print_info("Clearing Database...")
+      with open("game.db","w") as f:
+        f.write("")
+      print_info("Cleared Database")
+    else:
+      print_info("Creating Database...")
+      sqlite3.connect("game.db")
+      print_info("Created Database")
+    conn = sqlite3.connect("game.db")
+    with open("data.sql","r") as f:
+      conn.executescript(f.read())
+  else:
+    print_info("Exited")     
+
 # /===================================/
 #           HELPER FUNCTIONS
 # /===================================/
 def xor_cipher(string: str, key: str) -> str:
-    return ("").join(chr(ord(x) ^ ord(y)) for x, y in zip(string, itertools.cycle(key)))
+  return ("").join(chr(ord(x) ^ ord(y)) for x, y in zip(string, itertools.cycle(key)))
+
+def xor_and_base64(string: str, key: str):
+  base64.b64encode(xor_cipher(string,key))
+
+def print_info(text: str):
+  print(f"[INFO] {text}")
 
 print(f"""
    __________  _____                           ____       
@@ -43,7 +71,8 @@ GitHub: https://github.com/mel0n7/GDServerPy""")
 print("""
 Type a number
 1. Start Server
-2. Server Prefrences/Settings""")
+2. Server Prefrences/Settings
+3. Create/Reset Database""")
 option = int(input())
-option_functions = {1: start_server}
+option_functions = {1: start_server,3:reset_database}
 option_functions[option]()
